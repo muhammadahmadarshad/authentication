83ee81f00d45d247c9d28a9191a8fa46e1de4f41
muhammadahmadarshad <muhammadahmadarshad@hotmail.com>
2020-02-27 01:45:08 +0500

    final

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const User = require('./routes/userRoute')
 const mongoose = require('mongoose')
+const cors=require('cors')
 
 const server = http.createServer(express);
 
@@ -14,6 +15,7 @@ mongoose.connect(
     }
 ).then((res) => console.log('connected')).catch(err => console.log(err));
 
+app.use(cors())
 app.use(express.json())
 app.use('/',User)
 
